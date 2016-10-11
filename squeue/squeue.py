6a1a0512901be26ddeeb209dae63f75c993de2d0
Denis Minich <denis.minich.fitr@gmail.com>
2016-10-11 19:49:29 +0300

    Updated message structure in storage

--- squeue/squeue.py
+++ squeue/squeue.py
@@ -9,10 +9,19 @@ class Squeue(object):
     _MESSAGE_LENGTH_SIZE = 2
     """Size in bytes of info about message length"""
 
+    _MESSAGE_READ_FLAG_SIZE = 1
+    """Size in bytes of info about if message already read"""
+
     _MAX_MESSAGE_LENGTH = 2 ** (_MESSAGE_LENGTH_SIZE * 8)
     """Max possible size of message to write"""
 
-    _name = None
+    _MESSAGE_NOT_READ = 0
+    """Available value for flag if message hasn't been read yet"""
+
+    _MESSAGE_READ = 1
+    """Available value for flag if message has been read already"""
+
+    name = None
     """Name of the queue"""
 
     _storage = None
@@ -23,7 +32,7 @@ class Squeue(object):
 
     def _get_storage(self):
         """Returns storage for messages"""
-        return os.open(self._name, os.O_RDWR | os.O_CREAT)
+        return os.open(self.name, os.O_RDWR | os.O_CREAT)
 
     def __init__(self, name):
         """Class constructor
@@ -31,7 +40,7 @@ class Squeue(object):
         :param name: name of the queue
         :type name: str
         """
-        self._name = name
+        self.name = name
         self._storage = self._get_storage()
         self._read_position = 0
 
@@ -41,8 +50,9 @@ class Squeue(object):
         :param message: object to put
         :type message: any
         """
-        self._go_to_the_next_message()
+        self._go_to_the_write_position()
         message = self._serialize_message(message)
+        self._write_int(self._MESSAGE_NOT_READ, self._MESSAGE_READ_FLAG_SIZE)
         self._write_int(len(message), self._MESSAGE_LENGTH_SIZE)
         os.write(self._storage, message)
 
@@ -54,27 +64,38 @@ class Squeue(object):
         """
         self._go_to_the_read_position()
         while True:
-            message_length = self._read_int(self._MESSAGE_LENGTH_SIZE)
-            if message_length is None:
+            if self._is_end_of_file():
                 return None
-            if message_length:
-                message = self._read_bytes(message_length)
-                self._read_position += (
-                    message_length + self._MESSAGE_LENGTH_SIZE)
-                return self._deserialize_message(message)
-            self._go_to_the_next_message()
+            if self._message_already_read():
+                self._go_to_the_next_message()
+            else:
+                self._mark_message_as_read()
+                return self._read_message()
 
-    def _read_int(self, length):
+    def _read_message(self):
+        """Read message from storage"""
+        self._read_int(self._MESSAGE_READ_FLAG_SIZE)
+        message_length = self._read_int(self._MESSAGE_LENGTH_SIZE)
+        message = self._read_bytes(message_length)
+        return self._deserialize_message(message)
+
+    def _read_int(self, length, peek=False):
         """Read portion of data and converts it to int
 
         :param length: length in bytes of data
         :type length: int
+        :param peek: if True - cursor position doesn't changes
+        :type peek: bool
         :returns: value from file
         :rtype: int
         """
         data = os.read(self._storage, length)
         if not data:
             return None
+        if not peek:
+            self._read_position += length
+        else:
+            self._go_to_the_read_position()
         return int.from_bytes(data, byteorder='big')
 
     def _write_int(self, value, length):
@@ -88,16 +109,24 @@ class Squeue(object):
         converted_data = value.to_bytes(length, byteorder='big')
         os.write(self._storage, converted_data)
 
-    def _read_bytes(self, length):
+    def _read_bytes(self, length, peek=False):
         """Read portion of data and converts it to bytes
 
         :param length: length in bytes of data
         :type length: int
+        :param peek: if True - cursor position doesn't changes
+        :type peek: bool
         :returns: data from file
         :rtype: bytes
         """
         data = os.read(self._storage, length)
-        return data if data else None
+        if data is None:
+            return None
+        if not peek:
+            self._read_position += length
+        else:
+            self._go_to_the_read_position()
+        return data
 
     def _write_bytes(self, value):
         """Writes bytes value into file
@@ -117,10 +146,32 @@ class Squeue(object):
 
     def _go_to_the_next_message(self):
         """Change cursor to the next message"""
+        self._read_int(self._MESSAGE_READ_FLAG_SIZE)
         message_length = self._read_int(self._MESSAGE_LENGTH_SIZE)
         if message_length:
-            self._read_position += (
-                message_length + self._MESSAGE_LENGTH_SIZE)
+            self._read_position += message_length
+            self._go_to_the_read_position()
+
+    def _is_end_of_file(self):
+        """Checks if there is no messages in the storage
+
+        :returns: is end of file succeed
+        :rtype: bool
+        """
+        return self._read_int(self._MESSAGE_READ_FLAG_SIZE, peek=True) is None
+
+    def _message_already_read(self):
+        """Checks if next message already proceed
+
+        :returns: is message have been read already
+        :rtype: bool
+        """
+        return self._read_int(self._MESSAGE_READ_FLAG_SIZE, peek=True)
+
+    def _mark_message_as_read(self):
+        """Mark current message as proceeded"""
+        self._write_int(self._MESSAGE_READ, self._MESSAGE_READ_FLAG_SIZE)
+        self._go_to_the_read_position()
 
     @staticmethod
     def _serialize_message(message):
--- tests/test_squeue.py
+++ tests/test_squeue.py
@@ -2,12 +2,21 @@
 # pylint: disable=missing-docstring
 # pylint: disable=redefined-outer-name
 """Tests for SQueue"""
+import multiprocessing
 import os
 import tempfile
 
 import pytest
 
 from squeue.squeue import Squeue
+
+
+def put_data_to_queue(test_queue_name, data_queue):
+    Squeue(test_queue_name).put(data_queue.get())
+
+
+def get_data_from_queue(test_queue_name, data_queue):
+    data_queue.put(Squeue(test_queue_name).get())
 
 
 @pytest.fixture
@@ -23,10 +32,55 @@ def test_get_returns_message(queue):
     assert queue.get() == string_message
 
 
-def test_get_returns_message_in_right_sequence(queue):
+def test_get_works_after_put_to_the_same_queue(queue):
     first_message = "foo"
     second_message = "bar"
     queue.put(first_message)
     queue.put(second_message)
     assert queue.get() == first_message
     assert queue.get() == second_message
+
+
+def test_get_returns_message_in_right_sequence(queue):
+    first_message = "foo"
+    second_message = "bar"
+    second_queue = Squeue(queue.name)
+    queue.put(first_message)
+    assert second_queue.get() == first_message
+    second_queue.put(second_message)
+    assert queue.get() == second_message
+
+
+def test_put_not_changes_read_position(queue):
+    first_message = "spam"
+    second_message = "ham"
+    third_message = "eggs"
+    second_queue = Squeue(queue.name)
+    queue.put(first_message)
+    second_queue.put(second_message)
+    queue.put(third_message)
+    assert second_queue.get() == first_message
+    assert queue.get() == second_message
+    assert second_queue.get() == third_message
+
+
+def get_returns_message_from_another_process(queue):
+    message = "foo"
+    data_queue = multiprocessing.Queue()
+    get_process = multiprocessing.Process(
+        target=get_data_from_queue, args=(queue.name, data_queue))
+    get_process.start()
+    data_queue.put(message)
+    get_process.join()
+    assert queue.get() == message
+
+
+def put_writes_message_for_another_process(queue):
+    message = "foo"
+    data_queue = multiprocessing.Queue()
+    get_process = multiprocessing.Process(
+        target=put_data_to_queue, args=(queue.name, data_queue))
+    get_process.start()
+    queue.put(message)
+    get_process.join()
+    assert data_queue.get() == message
